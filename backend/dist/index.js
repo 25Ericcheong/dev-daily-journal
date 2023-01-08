7b3d6dc0e9b3049df5cb68c7e67e151358c69299
25Ericcheong <ericcheongkaikit@gmail.com>
2023-01-08 22:46:32 +1000

    a working version of backend - needed to ensure the npm commands were modified to allow typescript to work in the backend

---
+++ backend/dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const express_1 = tslib_1.__importDefault(require("express"));
+const dotenv_1 = tslib_1.__importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT;
+app.get("/", (req, res) => {
+    res.send("Express + TypeScript Server");
+});
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
