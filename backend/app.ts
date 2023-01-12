41facf0688505662c06ca7c91077742dbca301ce
25Ericcheong <ericcheongkaikit@gmail.com>
2023-01-12 22:36:50 +1000

    code to connect to database

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,16 @@
-import express, { Express, Request, Response } from "express";
+import { Express, Request, Response } from "express";
 
 import dotenv from "dotenv";
 
 dotenv.config();
 
+const express = require("express");
+const connectDB = require("./config/db");
+
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8082;
+
+connectDB();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Test test");
