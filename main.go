16c44cb7ecf98edbf437e2e650b67220c1906e9b
25Ericcheong <ericcheongkaikit@gmail.com>
2023-06-26 22:54:54 +1000

    started first server ever in go!

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	fmt.Println("Server is running")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Server has responded. You have requested %s\n", r.URL.Path)
+	})
+
+	http.ListenAndServe(":8000", nil)
 }
