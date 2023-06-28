e22f9200aac91790b47da67d87b9ba97a4940ca3
25Ericcheong <ericcheongkaikit@gmail.com>
2023-06-28 23:46:44 +1000

    testing to see how static files are served to the frontend

--- main.go
+++ main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	fmt.Println("Server is running")
 
+	dir := http.Dir("./static")
+	fs := http.FileServer(dir)
+	mux := http.NewServeMux()
+	mux.Handle("/test", fs)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Server has responded. You have requested %s\n", r.URL.Path)
 	})
 
-	http.ListenAndServe(":8000", nil)
+	err := http.ListenAndServe(":8000", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
