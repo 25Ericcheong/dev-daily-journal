8b534e71bd636cb4b6daea9e46144391ab79f5f6
25Ericcheong <ericcheongkaikit@gmail.com>
2023-07-06 22:24:11 +1000

    first ever respond with a template and also got rid of existing style

--- main.go
+++ main.go
@@ -2,25 +2,62 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
-func main() {
+func init() {
 	fmt.Println("Server is running")
+}
 
-	dir := http.Dir("./static")
-	fs := http.FileServer(dir)
+func main() {
+
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", index)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Server has responded. You have requested %s\n", r.URL.Path)
-	})
-	mux.Handle("/test", fs)
-
-	err := http.ListenAndServe(":8000", nil)
+	err := http.ListenAndServe(":8000", mux)
 
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Path)
+
+	const tpl = `
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="UTF-8">
+			<title>{{.Title}}</title>
+		</head>
+		<body>
+			{{.Text}}
+		</body>
+	</html>
+	`
+
+	switch r.Method {
+	case http.MethodGet:
+		t, err := template.New("home").Parse(tpl)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		data := struct {
+			Title string
+			Text  string
+		}{
+			Title: "Daily Dev Journal",
+			Text:  "Welcome to my first page with my first respond using the template library!",
+		}
+
+		err = t.Execute(w, data)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
