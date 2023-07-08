564a1270a21dd4d521b4a397a1c9b5488640d2ba
25Ericcheong <ericcheongkaikit@gmail.com>
2023-07-08 11:24:04 +1000

    initial setup for mongodb connection and added relevant dependencies - have yet to test

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Article struct {
@@ -18,10 +23,28 @@ func init() {
 
 func main() {
 
+	// mongodb related setup
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("MONGODB_URI environment variable is not found")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// preparing and establishing server
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", index)
 
-	err := http.ListenAndServe(":8000", mux)
+	err = http.ListenAndServe(":8000", mux)
 
 	if err != nil {
 		log.Fatal(err)
@@ -41,7 +64,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 		data := Article {
 			Title: "Daily Dev Journal",
-			Text: "Welcome to my first page with my first respond using the template library!",
+			Text: "Welcome to my first page with my first respond using the template library!!!?",
 		}
 
 		err = t.Execute(w, data)
