35193f196d255fdd4f3e3299ff792c5860289e74
25Ericcheong <ericcheongkaikit@gmail.com>
2023-08-22 21:16:40 +1000

    refactor code a little and used hel helper function for error checking

--- main.go
+++ main.go
@@ -19,27 +19,37 @@ type Journal struct {
 	Body string
 }
 
+const noPanic = false
+const mongoEnvUri = "MONGODB_URI"
+const port = ":8000"
+
 func init() {
 	fmt.Println("Server is running")
+}
+
+func checkErr(err error, toPanic bool) {
+	if err != nil {
+		if toPanic == true {
+			panic(err)
+		} else {
+			log.Fatal(err)
+		}
+    }
 }
 
 func main() {
 
 	// mongodb related setup
 	err := godotenv.Load()
-    if err != nil {
-    	log.Fatal(err)
-    }
+    checkErr(err, noPanic)
 
-	uri := os.Getenv("MONGODB_URI")
+	uri := os.Getenv(mongoEnvUri)
 	if uri == "" {
 		log.Fatal("MONGODB_URI environment variable is not found")
 	}
 
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
-	if err != nil {
-		panic(err)
-	}
+	checkErr(err, !noPanic)
 
 	defer func() {
 		if err := client.Disconnect(context.TODO()); err != nil {
@@ -53,11 +63,8 @@ func main() {
 	mux.HandleFunc("/", index)
 	mux.Handle("/html/styles/", http.StripPrefix("/html/styles/", http.FileServer(http.Dir("./html/styles"))))
 
-	err = http.ListenAndServe(":8000", mux)
-
-	if err != nil {
-		log.Fatal(err)
-	}
+	err = http.ListenAndServe(port, mux)
+	checkErr(err, noPanic)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -67,19 +74,18 @@ func index(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		temp, err := template.New("index.html").ParseFiles("./html/templates/index.html")
-
-		if err != nil {
-			log.Fatal(err)
-		}
+		checkErr(err, noPanic)
 
 		data := Journal {
 			Title: "Daily Dev Journal",
-			Body: "Test daemon watcher to my first taildwind work with my first respond using the template library again",
+			Body: "Temporary dummy data for not - will include data from MongoDB",
 		}
 
 		err = temp.Execute(w, data)
-		if err != nil {
-			log.Fatal(err)
-		}
+		checkErr(err, noPanic)
+
+	case http.MethodPost:
+		err := r.ParseForm()
+		checkErr(err, noPanic)
 	}
 }
